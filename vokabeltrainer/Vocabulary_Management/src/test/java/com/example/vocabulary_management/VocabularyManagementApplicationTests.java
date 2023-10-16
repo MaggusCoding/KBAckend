e0635b5a42735711607cea7e816aeddaa7e5f3af
murks1 <mseethaler98@gmail.com>
2023-10-16 16:43:00 +0200

    set up project structure

---
+++ vokabeltrainer/Vocabulary_Management/src/test/java/com/example/vocabulary_management/VocabularyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vocabulary_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
