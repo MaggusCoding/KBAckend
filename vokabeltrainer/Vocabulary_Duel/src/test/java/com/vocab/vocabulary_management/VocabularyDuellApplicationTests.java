48e3406a58789ae5fca9676aabff22e4bd44db5f
murks1 <mseethaler98@gmail.com>
2023-10-18 14:59:32 +0200

    doofi

---
+++ vokabeltrainer/Vocabulary_Duel/src/test/java/com/vocab/vocabulary_management/VocabularyDuellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocab.vocabulary_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyDuellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
