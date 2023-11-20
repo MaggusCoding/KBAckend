b392cc7dce3ebb1805af2d71d23e6b692a2c8a47
MK <Marin.Klein@Student.HTW-Berlin.de>
2023-11-20 16:04:36 +0100

    forgotten repository method

--- vokabeltrainer/Vocabulary_Management_API/src/main/java/com/vocab/vocabulary_management/repos/FlashcardListRepo.java
+++ vokabeltrainer/Vocabulary_Management_API/src/main/java/com/vocab/vocabulary_management/repos/FlashcardListRepo.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FlashcardListRepo extends JpaRepository<FlashcardList, Long> {
+    FlashcardList findByCategory(String category);
+
 }
