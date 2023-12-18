214c1294bac3b3c19dc35e5d74df28a5cf0cad34
MK <Marin.Klein@Student.HTW-Berlin.de>
2023-12-18 14:55:39 +0100

    refactoring calculateWinner - reduced streaming insanity

--- vokabeltrainer/Vocabulary_Duel_API/src/main/java/com/vocab/vocabulary_duel/repositories/DuelRepo.java
+++ vokabeltrainer/Vocabulary_Duel_API/src/main/java/com/vocab/vocabulary_duel/repositories/DuelRepo.java
@@ -1,7 +1,9 @@
 package com.vocab.vocabulary_duel.repositories;
 
+import com.vocab.vocabulary_duel.dto.RankingPlayer;
 import com.vocab.vocabulary_duel.entities.Duel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,12 @@ public interface DuelRepo extends JpaRepository<Duel, Long> {
 
     List<Duel> findDuelsByStartedIsFalseAndFinishedIsFalse();
 
+    @Query("select new com.vocab.vocabulary_duel.dto.RankingPlayer(u.username, count(u.username)) from UserEntity u " +
+            "join Duel d on d.finished = true " +
+            "join Round r on r.duel = d " +
+            "join Answer a on a.round = r and a.player = u " +
+            "where a.correct and d.duelId = :duelId " +
+            "group by u.username " +
+            "order by count(u.username) desc")
+    List<RankingPlayer> getRankingOfDuel(long duelId);
 }
