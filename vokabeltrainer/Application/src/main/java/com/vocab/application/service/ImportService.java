d4debe842577b3b536d221b21e6fd2e17638eaa2
MK <Marin.Klein@Student.HTW-Berlin.de>
2023-12-19 16:20:06 +0100

    javadoc importservice

--- vokabeltrainer/Application/src/main/java/com/vocab/application/service/ImportService.java
+++ vokabeltrainer/Application/src/main/java/com/vocab/application/service/ImportService.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 public interface ImportService {
 
     /**
-     * read a content of a file.
+     * read a content of a file. It is required to give the full absolute path of the file.
      *
-     * @param path
+     * @param path full absolute path of the file to be read.
      * @return content of file
      * @throws FileNotFoundException  if the file does not exist or no files in default path exists
      * @throws InvalidFormatException if the file(s) have not the expected format
@@ -18,7 +18,7 @@ public interface ImportService {
     boolean importFile(String path) throws IOException;
 
     /**
-     * reads initial flashcardlist files and save it to database
+     * reads initial flashcardlist files and save it to database. Default path is relative path to current location and it is required to have the folder 'vocabFiles'.
      * @return true if succeeded, false otherwise
      */
     boolean importInitialFiles() throws IOException;
