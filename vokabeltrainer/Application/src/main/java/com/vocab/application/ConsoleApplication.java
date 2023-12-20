bc965c751166aa0a8e99a6117345d8faa92423b6
MK <Marin.Klein@Student.HTW-Berlin.de>
2023-12-20 11:34:49 +0100

    userserviceImplTest auf mockito umgestellt

--- vokabeltrainer/Application/src/main/java/com/vocab/application/ConsoleApplication.java
+++ vokabeltrainer/Application/src/main/java/com/vocab/application/ConsoleApplication.java
@@ -22,10 +22,15 @@ import java.util.stream.Collectors;
 
 /**
  * TODO: 1. Try Catch Blöcke für alle Auswahlmöglichkeiten zwecks Exception Handling oder if Blöcke
- * 2. Javadoc überprüfen / Aufräumen Sachen die wir nicht brauchen
- * 3. Testfälle für alle Service Klassen neu machen mit Mockito siehe: https://github.com/MaggusCoding/webtechKassensystemBackend
- * (4.) Evtl. Model View Controller für Frontend evtl.
- * 7. Einfach mal weng rumzocken in der Anwendung
+ * 1. Javadoc überprüfen / Aufräumen Sachen die wir nicht brauchen
+ * 2. Testfälle für alle Service Klassen neu machen mit Mockito siehe: https://github.com/MaggusCoding/webtechKassensystemBackend
+ * 3. Model View Controller für Frontend.
+ * 4. Einfach mal wenig rumzocken in der Anwendung
+ * 5. Aufräumarbeiten(danach tests laufen lassen und 1x spielen):
+ *  5.1. Überflüssige Tests entfernen: Tests in den API-Modulen, ApplicationTests, FlashcardImplTest, TranslationImplTest...
+ *  5.2. überflüssige Services entfernen: FlashcardImpl, TranslationImpl...
+ *  5.3. src-Folder auf root-Ebene
+ *
  */
 @Component
 public class ConsoleApplication implements CommandLineRunner {
--- vokabeltrainer/User_Management_Impl/src/test/java/com/vocab/user_management_impl/UserServiceImplTest.java
+++ vokabeltrainer/User_Management_Impl/src/test/java/com/vocab/user_management_impl/UserServiceImplTest.java
@@ -3,26 +3,33 @@ package com.vocab.user_management_impl;
 import com.vocab.user_management.entities.UserEntity;
 import com.vocab.user_management.repos.UserRepo;
 import com.vocab.user_management_impl.services.UserServiceImpl;
-import org.junit.jupiter.api.AfterEach;
+import com.vocab.vocabulary_duel.repositories.DuelRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
 public class UserServiceImplTest {
 
-    @Autowired
+    @InjectMocks
     private UserServiceImpl userService;
 
-    @Autowired
+    @Mock
     private UserRepo userRepo;
+
+    @Mock
+    private DuelRepo duelRepo;
 
     private UserEntity user1;
     private UserEntity user2;
@@ -32,81 +39,127 @@ public class UserServiceImplTest {
 
         // Erstelle und füge einige Test-UserEntity-Objekte hinzu
         user1 = new UserEntity();
+        user1.setUserId(1L);
         user1.setUsername("testuser1");
-        userRepo.save(user1);
 
         user2 = new UserEntity();
+        user2.setUserId(2L);
         user2.setUsername("testuser2");
-        userRepo.save(user2);
-    }
 
-    @AfterEach
-    public void tearDown(){
-        userRepo.deleteAll(); // Setzt die Datenbank zurück
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
     public void testCreateUser() {
+        UserEntity user3 = new UserEntity();
+        user3.setUserId(3L);
+        user3.setUsername("testuser3");
+        when(userRepo.findByUsername("testuser1")).thenReturn(Optional.ofNullable(user1));
+        when(userRepo.findByUsername(user3.getUsername())).thenReturn(Optional.empty());
+        when(userRepo.save(any(UserEntity.class))).thenReturn(user3);
+
         // Erstelle einen neuen Benutzer
         UserEntity existingUser = userService.createUser("testuser1");
         UserEntity newUser = userService.createUser("testuser3");
+
         assertNotNull(newUser);
+        assertEquals(existingUser.getUserId(), user1.getUserId() );
         assertEquals(existingUser.getUsername(), user1.getUsername() );
-        assertEquals("testuser3", newUser.getUsername(), "Username stimmt nicht überein");
+        assertEquals(user3.getUserId(), newUser.getUserId(), "neuer User wurde nicht angelegt");
+        assertEquals(user3.getUsername(), newUser.getUsername(), "Username stimmt nicht überein");
+        verify(userRepo, times(2)).findByUsername(anyString());
+        verify(userRepo, times(0)).save(user1);
+        verify(userRepo).save(any(UserEntity.class));
     }
 
     @Test
     public void testFindByUsername() {
+        when(userRepo.findByUsername(user1.getUsername())).thenReturn(Optional.ofNullable(user1));
+
         // Versuche, den Benutzer über findByUsername zu finden
-        Optional<UserEntity> foundUser = userService.findByUsername("testuser1");
+        Optional<UserEntity> foundUser = userService.findByUsername(user1.getUsername());
 
         // Überprüfe, ob der Benutzer gefunden wurde und ob die Daten übereinstimmen
         assertTrue(foundUser.isPresent(), "Benutzer wurde nicht gefunden");
         assertEquals("testuser1", foundUser.get().getUsername(), "Benutzername stimmt nicht überein");
+        verify(userRepo).findByUsername(user1.getUsername());
     }
 
     @Test
     public void testDeleteUser() {
-        // Lösche einen Benutzer
-        userService.deleteUser(user1.getUserId());
+        when(userRepo.existsById(user1.getUserId())).thenReturn(true);
+        when(userRepo.findById(user1.getUserId())).thenReturn(Optional.of(user1));
+        when(duelRepo.existsDuelByPlayersIsContaining(user1)).thenReturn(false);
 
-        // Versuche, den Benutzer über findById zu finden
-        Optional<UserEntity> foundUser = userService.findByUsername("testuser1");
+        boolean deleteSuccess = userService.deleteUser(user1.getUserId());
 
-        // Überprüfe, ob der Benutzer nicht gefunden wurde
-        assertFalse(foundUser.isPresent(), "Benutzer wurde nicht gelöscht");
+        assertTrue(deleteSuccess);
+        verify(userRepo).existsById(user1.getUserId());
+        verify(userRepo).findById(user1.getUserId());
+        verify(duelRepo).existsDuelByPlayersIsContaining(user1);
+        verify(userRepo).deleteById(user1.getUserId());
     }
 
     @Test
     public void testDeleteNotPersistedUser(){
         // Erstelle User-Objekt, aber es wird nicht persistiert
         UserEntity newUser = new UserEntity();
-        newUser.setUserId(-1L);
+        newUser.setUserId(3L);
         newUser.setUsername("notPersistedUser");
+        when(userRepo.existsById(newUser.getUserId())).thenReturn(false);
+        when(userRepo.findById(newUser.getUserId())).thenReturn(Optional.of(newUser));
+        when(duelRepo.existsDuelByPlayersIsContaining(newUser)).thenReturn(false);
 
         // Führe Löschung durch
         boolean deleteSuccess = userService.deleteUser(newUser.getUserId());
 
         // Prüfe, dass nichts gelöscht wurde
-        assertFalse(deleteSuccess, "Benutzer wurde nicht gelöscht");
+        assertFalse(deleteSuccess, "Benutzer wurde gelöscht");
+        verify(userRepo).existsById(newUser.getUserId());
+        verify(userRepo, times(0)).findById(newUser.getUserId());
+        verify(duelRepo, times(0)).existsDuelByPlayersIsContaining(newUser);
+        verify(userRepo, times(0)).deleteById(newUser.getUserId());
+    }
+
+    @Test
+    public void testDeleteUserWhoPlaysInDuel(){
+        // Erstelle User-Objekt, aber es wird nicht persistiert
+        UserEntity newUser = new UserEntity();
+        newUser.setUserId(3L);
+        newUser.setUsername("player");
+        when(userRepo.existsById(newUser.getUserId())).thenReturn(true);
+        when(userRepo.findById(newUser.getUserId())).thenReturn(Optional.of(newUser));
+        when(duelRepo.existsDuelByPlayersIsContaining(newUser)).thenReturn(true);
+
+        // Führe Löschung durch
+        boolean deleteSuccess = userService.deleteUser(newUser.getUserId());
+
+        // Prüfe, dass nichts gelöscht wurde
+        assertFalse(deleteSuccess, "Benutzer wurde gelöscht");
+        verify(userRepo, times(0)).deleteById(newUser.getUserId());
+        verify(duelRepo, times(1)).existsDuelByPlayersIsContaining(newUser);
+        verify(userRepo, times(1)).existsById(newUser.getUserId());
     }
 
     @Test
     public void testGetById() {
+        when(userRepo.findById(user1.getUserId())).thenReturn(Optional.ofNullable(user1));
+
         UserEntity foundUser = userService.getById(user1.getUserId());
 
         // Überprüfe, ob der abgerufene Benutzer korrekt ist
         assertNotNull(foundUser, "Benutzer wurde nicht gefunden");
         assertEquals(user1.getUsername(), foundUser.getUsername(), "Benutzernamen stimmen nicht überein");
+        verify(userRepo).findById(user1.getUserId());
     }
 
     @Test
     public void testGetAll() {
         // Füge hier mehrere Benutzer hinzu und überprüfe, ob alle abgerufen werden können
         UserEntity newUser = new UserEntity();
+        newUser.setUserId(3L);
         newUser.setUsername("testuser3");
-        // Setze hier weitere benötigte Felder
-        userRepo.save(newUser);
+        when(userRepo.findAll()).thenReturn(List.of(user1, user2, newUser));
 
         List<UserEntity> users = userService.getAll();
 
@@ -114,5 +167,6 @@ public class UserServiceImplTest {
         assertTrue(users.size() >= 2, "Nicht alle Benutzer wurden abgerufen");
         assertTrue(users.stream().anyMatch(u -> u.getUsername().equals("testuser1")), "Benutzer 1 wurde nicht abgerufen");
         assertTrue(users.stream().anyMatch(u -> u.getUsername().equals("testuser2")), "Benutzer 2 wurde nicht abgerufen");
+        verify(userRepo).findAll();
     }
 }
