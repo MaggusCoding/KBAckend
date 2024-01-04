e3d32fb5658de138a4feabc7986206e4ff1c77e1
MaggusCoding <mseethaler98@gmail.com>
2024-01-04 20:15:03 +0100

    added correct pom for Application

---
+++ vokabeltrainer/Application_UI_API/src/main/java/com/vocab/application_ui_api/ApplicationUiApiApplication.java
@@ -0,0 +1,13 @@
+package com.vocab.application_ui_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationUiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationUiApiApplication.class, args);
+    }
+
+}
---
+++ vokabeltrainer/Application_UI_Impl/src/main/java/com/vocab/application_ui_impl/ApplicationUiImplApplication.java
@@ -0,0 +1,13 @@
+package com.vocab.application_ui_impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationUiImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationUiImplApplication.class, args);
+    }
+
+}
